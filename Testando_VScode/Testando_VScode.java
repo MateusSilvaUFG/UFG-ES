cd427ef281d02c0f894fe41d9b2b08818ec3dfc0
Mateus Sousa <132518094+BushOnGekko@users.noreply.github.com>
2023-07-03 15:59:56 -0300

    Teste_VScode

---
+++ Testando_VScode/Testando_VScode.java
@@ -0,0 +1,11 @@
+package Testando_VScode;
+
+public class Testando_VScode{
+    public static void main(String args[]){
+        System.out.println("OLá Vscode");
+    
+        Pessoa Humano = new Pessoa("mateus", 24, "70549804170  ");
+
+        System.out.println(Humano.nome);
+    }
+}
